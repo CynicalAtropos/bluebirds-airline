481e77d6342b571e6a6e6d6f2399a7066c54babc
tdambp <tdambp@localhost>
2017-10-04 09:48:24 -0400

    Fixed Customer

--- src/bluebirds/airline/Customer.java
+++ src/bluebirds/airline/Customer.java
@@ -1,20 +1,25 @@
+package bluebirds.airline;
+
+import java.util.ArrayList;
 
 public class Customer extends Person{
 	private int customerId;
-        
+        private ArrayList<Reservation> reservationList;
         public static int num = 100;
 	
-	public Customer(String name, String address, String phoneNumber)
+	public Customer(String name, String address, String phoneNumber, ArrayList<Reservation> r)
 	{
 		super(name,address,phoneNumber);
 		customerId = num;
                 num++;
+                reservationList = r;
 	}
 	
 	public Customer()
 	{
             customerId = num;
             num++;
+            
 		
 	}
 	
@@ -33,5 +38,17 @@ public class Customer extends Person{
 		this.customerId = customerId;
 	}
 	
+        public void addRes(Reservation r) {
+            this.reservationList.add(r);
+        }
+        
+        public void printRes(){
+            System.out.println("\nReservations for " + this.name);
+            for(int i = 0; i < this.reservationList.size(); i++){
+                System.out.println(this.reservationList.get(i).getFlight() + 
+                        this.reservationList.get(i).getSeatNumber() +
+                        this.reservationList.get(i).getCost());
+            }
+        }
 	
 }
