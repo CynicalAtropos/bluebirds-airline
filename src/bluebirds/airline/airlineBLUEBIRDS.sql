d0bdfe157507fe7d325fd7ac6dda7858e48ce876
slash <slash@10.80.11.37>
2017-11-07 09:53:27 -0500

    tables created

--- src/bluebirds/airline/airlineBLUEBIRDS.sql
+++ src/bluebirds/airline/airlineBLUEBIRDS.sql
@@ -1,3 +1,91 @@
+drop table if exists canceledreservations;
+drop table if exists customers;
+drop table if exists flights;
+drop table if exists pilots;
+drop table if exists reservations;
+drop table if exists seatmap;
+
+CREATE TABLE IF NOT EXISTS `canceledreservations` (
+  `resID` int(11) NOT NULL,
+  `custID` int(11) NOT NULL,
+  `seatNumber` varchar(10) NOT NULL,
+  `firstClass` int(1) NOT NULL,
+  `flightCode` varchar(15) NOT NULL,
+  `cost` int(10) NOT NULL,
+  PRIMARY KEY (`resID`),
+  KEY `custID` (`custID`),
+  KEY `flightCode` (`flightCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `customers` (
+  `custID` int(11) NOT NULL,
+  `customerName` varchar(32) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  PRIMARY KEY (`custID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `flights` (
+  `flightCode` varchar(15) NOT NULL,
+  `flightDay` date NOT NULL,
+  `flightTime` varchar(5) NOT NULL,
+  `route` varchar(50) NOT NULL,
+  `pilotID` int(11) NOT NULL,
+  PRIMARY KEY (`flightCode`),
+  KEY `pilotID` (`pilotID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `pilots` (
+  `pilotID` int(11) NOT NULL,
+  `pilot` varchar(32) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  PRIMARY KEY (`pilotID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `reservations` (
+  `resID` int(11) NOT NULL,
+  `custID` int(11) NOT NULL,
+  `seatNumber` varchar(10) NOT NULL,
+  `firstClass` int(1) NOT NULL,
+  `flightCode` varchar(15) NOT NULL,
+  `cost` int(10) NOT NULL,
+  PRIMARY KEY (`resID`),
+  KEY `custID` (`custID`),
+  KEY `flightCode` (`flightCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `seatmap` (
+  `flightCode` varchar(6) NOT NULL,
+  `FCA1` int(3) DEFAULT NULL,
+  `FCA2` int(3) DEFAULT NULL,
+  `FCB1` int(3) DEFAULT NULL,
+  `FCB2` int(3) DEFAULT NULL,
+  `ECA1` int(3) DEFAULT NULL,
+  `ECA2` int(3) DEFAULT NULL,
+  `ECA3` int(3) DEFAULT NULL,
+  `ECA4` int(3) DEFAULT NULL,
+  `ECB1` int(3) DEFAULT NULL,
+  `ECB2` int(3) DEFAULT NULL,
+  `ECB3` int(3) DEFAULT NULL,
+  `ECB4` int(3) DEFAULT NULL,
+  KEY `flightCode` (`flightCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `canceledreservations`
+  ADD CONSTRAINT `canceledreservations_ibfk_1` FOREIGN KEY (`custID`) REFERENCES `customers` (`custID`),
+  ADD CONSTRAINT `canceledreservations_ibfk_2` FOREIGN KEY (`flightCode`) REFERENCES `flights` (`flightCode`);
+
+ALTER TABLE `flights`
+  ADD CONSTRAINT `flights_ibfk_1` FOREIGN KEY (`pilotID`) REFERENCES `pilots` (`pilotID`);
+
+ALTER TABLE `reservations`
+  ADD CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`custID`) REFERENCES `customers` (`custID`),
+  ADD CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`flightCode`) REFERENCES `flights` (`flightCode`);
+
+ALTER TABLE `seatmap`
+  ADD CONSTRAINT `seatmap_ibfk_1` FOREIGN KEY (`flightCode`) REFERENCES `flights` (`flightCode`);
+
 DROP PROCEDURE IF EXISTS searchReservID;
 CREATE PROCEDURE searchReservID(resNum INT(3))
 SELECT r.ResID, c.Name, r.SeatNum, r.FlightCode, r.Cost
